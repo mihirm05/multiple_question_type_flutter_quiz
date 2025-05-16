923196ef2d53c656001fd6e68dc5298e8c60d5ab
mihirm05 <mihirmulye95@gmail.com>
2025-05-16 07:44:22 +0200

    create a config file

---
+++ lib/config.dart
@@ -0,0 +1,11 @@
+String animationsFolder = 'animations';
+
+String patientTherapist = '$animationsFolder/therapistIcon.png';
+
+// variety of rival expressions
+String r1Bloat1 = '$animationsFolder/R1_bloat_1.png';  
+String r1Angry1 = '$animationsFolder/R1_angry_1.png';
+String r1Content1 = '$animationsFolder/R1_content_1.png';
+String r1Dissatisfied1 = '$animationsFolder/R1_dissatisfied_1.png';
+String r1Expecting1 = '$animationsFolder/R1_expecting_1.png';
+
